43f0686df6d7123abe465b0245a6657243e20248
HemaElangovan <88083007+HemaElangovan@users.noreply.github.com>
2021-07-28 16:05:53 +0530

    Add files via upload

---
+++ ElecBC.cs
@@ -0,0 +1,49 @@
+using System;
+
+
+class ElectricityBillCal
+
+{
+   
+public static void  Main() 
+    
+{
+        
+Console.WriteLine("Please enter the units");
+        
+int units = Convert.ToInt32(Console.ReadLine());
+        
+if(units <=200)
+        
+{
+            
+Console.WriteLine(units*2);
+        
+}
+        
+else if(units<350)
+        
+{
+            
+Console.WriteLine((200*2) + (units-200)*3);
+        
+}
+        else if(units<=500)
+        
+{
+           
+Console.WriteLine((200*2) + (200*3) + (units-350)*5);
+        
+}
+        
+else if(units >500)
+        
+{
+            
+Console.WriteLine((200*2) + (200*3) + (200*5) + (units-500) * 7);   
+        
+}
+    
+}
+
+}
