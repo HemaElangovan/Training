6839681f5a757993140e2fa5ce45f1ca1a543682
HemaElangovan <88083007+HemaElangovan@users.noreply.github.com>
2021-08-03 17:27:16 +0530

    Add files via upload

---
+++ Task-D5 (03-8-2021)/DesertsColl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingTask5
+{
+    class DesertsColl
+    {
+            static void Main()
+            {
+                ArrayList list = new ArrayList();
+                list.Add("White chocolate cake");
+                list.Add("Royal red velvet cake");
+                list.Add("Lemon Souffles");
+                list.Add("Chocolate mousse");
+                list.Add("Custard pudding");
+                FileStream fs = new FileStream("D:/Training/Task- D5(03-08-2021)/desert.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                StreamWriter sw = new StreamWriter(fs);
+                foreach (string s in list)
+                {
+                    sw.Write(s + "\n");
+                }
+                sw.Flush(); // Releases memory from RAM
+            sw.Close();
+            fs.Close();
+            FileInfo f1 = new FileInfo("D:/Training/Task- D5(03-08-2021)/desert.txt");
+            Console.WriteLine("The creation time of the file is " + f1.CreationTime +".");
+            Console.WriteLine("The length of the file is " + f1.Length + ".");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Task-D5 (03-8-2021)/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingTask5
+{
+    [Serializable]
+    class Employee
+    {
+        int EmpID;
+        string Name;
+        string Gender;
+        int Age;
+        public Employee(int id, string name, string gender, int age)
+        {
+            EmpID = id;
+            Name = name;
+            Gender = gender;
+            Age = age;
+        }
+        public static void Main()
+        {
+            Employee emp = new Employee(1010, "Jenifer", "Female", 12);
+            FileStream fs = new FileStream("D:/Training/Task- D5(03-08-2021)/EmployeeData.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, emp);
+            fs.Close();
+        
+            FileStream fs1 = new FileStream("D:/Training/Task- D5(03-08-2021)/EmployeeData.txt", FileMode.Open, FileAccess.Read);           
+            Employee e = (Employee)bf.Deserialize(fs1);
+            Console.WriteLine(e.EmpID);
+            Console.WriteLine(e.Name);
+            Console.WriteLine(e.Gender);
+            Console.WriteLine(e.Age);
+            Console.Read();
+        }
+    }
+}
