72303673117ea047f319b69cf9c238f27c714d27
HemaElangovan <88083007+HemaElangovan@users.noreply.github.com>
2021-08-23 19:11:20 +0530

    Add files via upload

---
+++ Task-D20 (23-08-2021)/RegisterController.cs
@@ -0,0 +1,63 @@
+﻿using MvcRegistrationTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcRegistrationTask.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        RegisterModel rm = new RegisterModel();
+        public ActionResult Get()
+        {
+            DataTable dt = rm.GetData();
+            return View("RFDetails",dt);
+        }
+        public ActionResult Insert()
+        {
+            return View ("InsertData");
+        }
+        public ActionResult InsertDetails(FormCollection fc, string action)
+        {
+            DataTable dt = rm.GetData();
+            if(action=="Submit")
+            {
+                string fname = fc["txt_fname"];
+                string lname = fc["txt_lname"];
+                string age = fc["txt_age"];
+                string gender = fc["txt_gender"];
+                int ins = rm.InsertData(fname, lname, Convert.ToInt32(age), gender);
+                return View("RFDetails", dt);
+            }
+            else
+            {
+                return RedirectToAction("Get", dt);
+            }
+        }
+        public ActionResult Edit(int rno)
+        {
+            DataTable dt = rm.Edit(rno);
+            return View("Edit", dt);
+
+        }
+        public ActionResult EditDetails(FormCollection fc, string action)
+        {
+            if(action =="Submit")
+            {
+                int rno = Convert.ToInt32(fc["txt_rno"]);
+                string fname = fc["txt_fname"];
+                string lname = fc["txt_lname"];
+                string age = fc["txt_age"];
+                string gender = fc["txt_gender"];
+                int ins = rm.EditData(rno,fname, lname, Convert.ToInt32(age), gender);
+                return RedirectToAction("Get");
+            }
+            return RedirectToAction("Get");
+        }
+    }
+}
---
+++ Task-D20 (23-08-2021)/RegisterModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MvcRegistrationTask.Models
+{
+    public class RegisterModel
+    {
+        SqlConnection con;
+        public RegisterModel()
+    {
+        con = new SqlConnection("Data Source=HEMA-E; Database =RegistrationDb;User ID=sa;Password=sasa@123");
+    }
+        public DataTable GetData()
+        {
+            string str = "select Rno, FirstName, LastName, Age, Gender from tbl_RegisterForm";
+            SqlDataAdapter da = new SqlDataAdapter(str, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        public int InsertData(string fn, string ln, int age, string gender)
+        {
+            string insert = "insert into tbl_RegisterForm(FirstName, LastName, Age, Gender) values (@fn,@ln,@age,@gender)";
+            SqlCommand cmd = new SqlCommand(insert, con);
+            cmd.Parameters.AddWithValue("@fn", fn);
+            cmd.Parameters.AddWithValue("@ln", ln);
+            cmd.Parameters.AddWithValue("@age", age);
+            cmd.Parameters.AddWithValue("@gender", gender);
+            con.Open();
+            return cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public DataTable Edit(int rno)
+        {
+            string dataedit = "select Rno,FirstName,LastName,Age,Gender from tbl_RegisterForm where Rno=" + rno;
+            SqlDataAdapter da = new SqlDataAdapter(dataedit, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        public int EditData(int rno, string fn, string ln, int age,string gender)
+        {
+            string up = "update tbl_RegisterForm set FirstName=@fn, LastName=@ln, Age=@age, Gender=@gender where Rno=@rno";
+            SqlCommand cmd = new SqlCommand(up, con);
+            cmd.Parameters.AddWithValue("@rno", rno);
+            cmd.Parameters.AddWithValue("@fn", fn);
+            cmd.Parameters.AddWithValue("@ln", ln);
+            cmd.Parameters.AddWithValue("@age", age);
+            cmd.Parameters.AddWithValue("@gender", gender);
+            con.Open();
+            int res = cmd.ExecuteNonQuery();
+            con.Close();
+            return res;
+        }
+
+    }
+}
